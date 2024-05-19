de082daecfa70d69bd12bd630683f46b0be7d676
ishaq-p <sultan.ishaq.321@gmail.com>
2024-05-20 00:03:31 +0300

    Help page Edited

--- src/UserInterface/Help.java
+++ src/UserInterface/Help.java
@@ -15,26 +15,26 @@ public class Help extends JPanel {
         add(title, BorderLayout.NORTH);
 
         // Create the content for the help page
-        JTextArea helpText = new JTextArea(){{setFont(new Font("MuseoSans-900", Font.BOLD, 20));}};
+        JTextArea helpText = new JTextArea(){{setFont(new Font("MuseoSans-900", Font.PLAIN, 20));}};
         helpText.setEditable(false);
         helpText.setLineWrap(true);
         helpText.setWrapStyleWord(true);
         helpText.setText(
             "Welcome to the Turkish Airlines User Interface Help Page!\n\n" +
             "Here you can find information on how to use the application:\n\n" +
-            "1. **Booking a Flight:**\n" +
-            "   - Go to the 'Book a Flight' tab.\n" +
+            "1. Booking a Flight:\n" +
+            "   - Go to the the home page or 'Book&Plane' tab.\n" +
             "   - Enter your departure and destination cities.\n" +
             "   - Select your travel dates and number of passengers.\n" +
             "   - Click 'Search' to find available flights.\n\n" +
-            "2. **Managing Bookings:**\n" +
-            "   - Go to the 'Manage Booking' tab.\n" +
-            "   - Enter your booking reference and last name.\n" +
-            "   - Click 'Retrieve Booking' to view or modify your booking.\n\n" +
-            "3. **Checking In:**\n" +
-            "   - Go to the 'Check-In' tab.\n" +
-            "   - Enter your booking details to check in online.\n" +
-            "   - Print your boarding pass or save it to your mobile device.\n\n" +
+            "2. Miles&Smiles Membership :\n" +
+            "   - Go to the 'Sign up' tab.\n" +
+            "   - Enter your Personal Info.\n" +
+            "   - Click 'Create Account' to become a member.\n\n" +
+            "3. Log in:\n" +
+            "   - Go to the 'Login' tab.\n" +
+            "   - Enter your Credentials if you have an account otherwise try the Sign Up tab.\n" +
+            "   - press login to give you all the access for a Miles&Smiles member\n\n" +
             "For further assistance, please contact our customer support."
         );
 
